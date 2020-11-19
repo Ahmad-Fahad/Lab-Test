b8b7c5f855a588b9c71b9b71e0ca352f85a1f323
Ahmad-Fahad <ahmadsmfahad@gmail.com>
2020-11-19 21:37:03 +0600

    confusing 2 problems

---
+++ Exams/assign_increment.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m;
+    int i = -1, j=-1, k=0, l=2;
+    /*
+    m = i++;
+    cout<<m<<endl<<i<<endl;
+    */
+    m = i++ && j++ && k++ || l++;
+    cout<<m<<endl;
+
+    int x = 3, y = 7;
+    int z = x && y;
+    cout<<z<<endl;
+
+}
---
+++ Exams/buet_exam.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define x 9+2/4*3-2*4+(5-4)*3
+#define v 4.3
+
+//  #define x 5.5
+
+int main()
+{
+    int i,y;
+    cout<<typeid(x).name()<<endl;
+    cout<<typeid(v).name()<<endl;
+    cout<<x<<endl;
+    y = 6+3*3/5;
+
+    cout<<x*x<<endl;
+    cout<<y<<endl;
+    i = x*x+y;
+    printf("%d",i);
+}
